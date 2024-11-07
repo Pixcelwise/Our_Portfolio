8423695813b0941570b8eeb8f51f233e409e22ab
unknown <sethtanmay72@gmail.com>
2024-11-07 14:45:29 +0530

    banner update

--- src/components/Wallpaper/Navbar/Navbar.jsx
+++ src/components/Wallpaper/Navbar/Navbar.jsx
@@ -1,65 +1,20 @@
-import { useEffect, useState } from "react";
-import styles from "./Navbar.module.css";
+import styles from './Navbar.module.css';
+import logo from '/logo.png'
 
-// eslint-disable-next-line react/prop-types
-function Navbar({ navbarStyle, color }) {
-    const [scrolled, setScrolled] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > window.innerHeight * 1.00) { // 1vh in pixels
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-
-        // Clean up the event listener on component unmount
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
+function Navbar() {
     return (
-        <nav
-            className={`navbar navbar-expand-lg ${styles[navbarStyle]} navbar-light  ${scrolled ? "navbar-light" : "navbar-light"} `}
-            style={{ color: color }}
-            id={styles.navbar}
-        >
-            {/* Knot Shots  logo  */}
-            <a className="navbar-brand" id={styles.brand_name} href="/">
-                {/* <img src={scrolled ? logoBlack : logoWhite} id={styles.brand_Logo} alt="" /> */}
-                <h3>Pixcelwise</h3>
-            </a>
-
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav" id={styles.navbar_buttons}>
-                    <li className="nav-item active">
-                        <a className="nav-link" id={styles.nav_link} href="/">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id={styles.nav_link} href="/gallery">PHOTOGRAPHY</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id={styles.nav_link} href="/films">FLIMS</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id={styles.nav_link} href="/contact">CONTACT</a>
-                    </li>
-                </ul>
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <img src={logo} alt="Travel Pulse Logo" />
+            </div>
+            <ul className={styles.navLinks}>
+                <li><a href="#destination">Destination</a></li>
+                <li><a href="#stories">Stories</a></li>
+                <li><a href="#reviews">Reviews</a></li>
+            </ul>
+            <div className={styles.authButtons}>
+                <button className={styles.signUp}>Sign Up</button>
+                <button className={styles.login}>Login</button>
             </div>
         </nav>
     );
--- src/components/Wallpaper/Top_Banner/Banner.jsx
+++ src/components/Wallpaper/Top_Banner/Banner.jsx
@@ -1,24 +1,32 @@
-import Navbar from '../Navbar/Navbar'
-import styles from './Banner.module.css'
-import img1 from "/Top_Banner/Top_image_1.svg"
-import img2 from "/Top_Banner/Top_image_2.svg"
-import img3 from "/Top_Banner/Top_image_3.svg"
+import Navbar from '../Navbar/Navbar';
+import styles from './Banner.module.css';
+import img1 from '/Top_Banner/Top_image_1.svg';
+import img2 from '/Top_Banner/Top_image_2.svg';
+import img3 from '/Top_Banner/Top_image_3.svg';
+
 function Banner() {
     return (
-        <section className={styles.Top_Banner_Container} >
+        <section className={styles.Top_Banner_Container}>
             <Navbar />
-            <div className={styles.img_container}>
-                <img src={img1} className={styles.Top_Banner_img} alt="top_banner_img" />
+            <div className={styles.overlay}>
+                <h1>Discover New Places and Create Unforgettable Memories</h1>
             </div>
-            <div className={styles.img_container}>
-                <img src={img2} className={styles.Top_Banner_img} alt="top_banner_img" />
+            <div className={styles.columns}>
+                <div className={styles.column}>
+                    <img src={img1} className={styles.image} alt="Inca Trail, Peru" />
+                    <p>Why you should reconsider The Inca Trail, Peru Trip</p>
+                </div>
+                <div className={styles.column}>
+                    <img src={img2} className={styles.image} alt="Northern Lights, Iceland" />
+                    <p>The Hidden Powers of The Northern Lights, Iceland</p>
+                </div>
+                <div className={styles.column}>
+                    <img src={img3} className={styles.image} alt="Blue Hole, Belize" />
+                    <p>10 Facts you didn’t know about The Blue Hole, Belize</p>
+                </div>
             </div>
-            <div className={styles.img_container}>
-                <img src={img3} className={styles.Top_Banner_img} alt="top_banner_img" />
-            </div>
-
         </section>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
