4b460eafe2613287e7fb8f928eb469bef1566201
Vikas Thakur <83107505+vikasthakur70183@users.noreply.github.com>
2024-11-07 14:41:00 +0530

    Update the top banner

--- src/components/Wallpaper/Top_Banner/Banner.jsx
+++ src/components/Wallpaper/Top_Banner/Banner.jsx
@@ -5,19 +5,25 @@ import img2 from "/Top_Banner/Top_image_2.svg"
 import img3 from "/Top_Banner/Top_image_3.svg"
 function Banner() {
     return (
-        <section className={styles.Top_Banner_Container} >
+        <section className={styles.Top_Banner_Container}>
             <Navbar />
+            <h1>Discover New Places and Create Unforgettable Memories</h1>
             <div className={styles.img_container}>
-                <img src={img1} className={styles.Top_Banner_img} alt="top_banner_img" />
+                <div className={styles.Top_Banner_img}>
+                    <img src={img1} alt="top_banner_img" />
+                    <p>Why you should reconsider The Inca Trail, Peru Trip <a href="#">→</a></p>
+                </div>
+                <div className={styles.Top_Banner_img}>
+                    <img src={img2} alt="top_banner_img" />
+                    <p>Why you should reconsider The Inca Trail, Peru Trip <a href="#">→</a></p>
+                </div>
+                <div className={styles.Top_Banner_img}>
+                    <img src={img3} alt="top_banner_img" />
+                    <p>Why you should reconsider The Inca Trail, Peru Trip <a href="#">→</a></p>
+                </div>
             </div>
-            <div className={styles.img_container}>
-                <img src={img2} className={styles.Top_Banner_img} alt="top_banner_img" />
-            </div>
-            <div className={styles.img_container}>
-                <img src={img3} className={styles.Top_Banner_img} alt="top_banner_img" />
-            </div>
-
         </section>
+
     )
 }
 
